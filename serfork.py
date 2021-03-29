4ae33bdb3447a43cff805ba1eea92b60e5c39aac
Toranova <chia_jason96@live.com>
2021-03-29 18:28:44 +0800

    added firewall config function

--- serfork.py
+++ serfork.py
@@ -4,6 +4,16 @@
 # mailto:chia_jason96@live.com
 import serial, subprocess, threading, time, socket, argparse, os
 
+def make_ipmasq_cmd(intif, lanif):
+    return [
+            ['/usr/bin/sysctl','net.ipv4.ip_forward=1'],
+            ['/usr/bin/iptables','-F'], # flush all entries
+            ['/usr/bin/iptables','-X'], # delete all chains
+            ['/usr/bin/iptables','-t','nat','-A','POSTROUTING','-o',intif,'-j','MASQUERADE'],
+            ['/usr/bin/iptables','-A','FORWARD','-m','conntrack','--cstate','RELATED,ESTABLISHED','-j','ACCEPT'],
+            ['/usr/bin/iptables','-A','FORWARD','-i',lanif,'-o',intif,'-j','ACCEPT'],
+    ]
+
 class SerialFork:
 
     # the debug and none debug functions are split for possible performance issues.
@@ -71,6 +81,7 @@ class SerialFork:
         self.pty0name = '/tmp/serfork0'
         self.pty1name = '/tmp/serfork1'
         self.baud = baud
+        self.uplink = uplink
         try:
             self.proc = subprocess.Popen([
                 '/usr/bin/socat',
@@ -86,7 +97,7 @@ class SerialFork:
             self.pty0 = serial.Serial(self.pty0name, baudrate = self.baud, timeout = timeout[1])
             self.pty1 = serial.Serial(self.pty1name, baudrate = self.baud, timeout = timeout[2])
             self.us = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-            if uplink:
+            if self.uplink:
                 if self.debug:
                     print("starting uplink device with udp service %s." % str(udpaddr))
                 self.us.bind(udpaddr)
@@ -106,16 +117,29 @@ class SerialFork:
             print("setup exception has occurred:",e)
             self.fatal = True
 
-    def run_until_interrupt(self, srcaddr, destaddr):
+    def run_until_interrupt(self, iface=None, srcaddr, destaddr):
         if self.fatal:
             raise Exception("setup exception was fatal.")
         try:
             self.start_forward()
-            p = subprocess.Popen([
+            ppp = subprocess.Popen([
                 '/usr/bin/pppd', 'nodetach', 'noauth', 'local',
                 '%s:%s' % (srcaddr, destaddr), self.pty0name, str(self.baud),
             ], stdout=subprocess.PIPE, universal_newlines=True)
-            for line in iter(p.stdout.readline, ""):
+
+            if iface is not None:
+                # configure firewall
+                if self.uplink: # uplink means iface is wlan0 or eth0, allow ppp0 to have internet
+                    iptab = make_ipmasq_cmd(iface, 'ppp0')
+                else: # downlink, share internet obtained from ppp0 through usb0 or some interface
+                    iptab = make_ipmasq_cmd('ppp0', iface)
+                for cmd in iptab:
+                    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr, subprocess.PIPE)
+                    out,err = p.communicate()
+                    if p.returncode: # error
+                        raise Exception("firewall exception was fatal. (%d) %s"%(p.returncode,err))
+
+            for line in iter(ppp.stdout.readline, ""):
                 print(line, end="")
         except KeyboardInterrupt:
             self.stop_forward()
@@ -127,6 +151,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("physical", help="the physical serial port to switch")
     parser.add_argument("-b", "--baudrate", help="the baud rate for all serial device", type=int, default=115200)
+    parser.add_argument("-i", "--interface", help="configure firewall for interface to share(downlink)/receive(uplink) internet")
     parser.add_argument("-d", "--debug", help="enable debugging mode (might affect performance)", action="store_true")
     parser.add_argument("-u", "--uplink", help="specify if this device is uplink. uplink is the udp server", action="store_true")
     parser.add_argument("-a", "--uaddr", help="uplink udp address (please specify if device is downlink)", default="0.0.0.0")
@@ -136,4 +161,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     sf = SerialFork(args.physical, args.uplink, (args.uaddr, args.uport), args.baudrate, args.debug)
-    sf.run_until_interrupt(args.ppp_src, args.ppp_dest)
+    sf.run_until_interrupt(argis.interface, args.ppp_src, args.ppp_dest)
